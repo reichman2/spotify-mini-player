431e437f4cf7265f5dc9d25918474a21ffb08f93
Brian Reich <reichman213@gmail.com>
2021-10-20 08:53:36 -0600

    add button to test spotify oauth flow

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
+import { Button } from './components/Button';
 import Controls from './components/Controls';
+
+import { authorize } from './oauth/auth';
 
 import './styles/main.scss';
 
 
 export function App() {
+  
+
   return (
     <>
       <Controls />
+      <Button onClick={ () => authorize() }>Authorize</Button>
     </>
-  )
+  );
 }
